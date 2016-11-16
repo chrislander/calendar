bc41fc2a07869dbd14ae4f8c7b358d1dde0bf7f8
Chris Lander <chrislander@gmail.com>
2016-11-16 11:33:37 +0000

    Removed call to parent constructor

--- calendar.php
+++ calendar.php
@@ -1,112 +1,110 @@
 <?php
 
-class calendar extends base {
-   
+class calendar {
+
     public $calendar;
-    
-    public function __construct($fregistry){                                
-        //Construct a calendar object and set a few accessible values
-        parent::__construct($fregistry);
+
+    public function __construct(){
 
         //Set readable calendar things, months, days etc
-        $this->set_readable_names();                                     
+        $this->set_readable_names();
     }
-    
-    
+
+
     public function show_day($year = false, $month = false, $day = false){
         if ( $year === false || $month === false  || $day === false ){
             //Show today
         }
     }
-    
-    public function show_month($year = false, $month = false) {
-        
-        if ( $year === false || $month === false ){  
-            $year = date('Y');
-            $month = date('m');            
-        }             
 
-        $no_days = cal_days_in_month(CAL_GREGORIAN, $month, $year);            
-        $date = new DateTime($year.'-'.$month.'-01');                                                                        			                                                                    
-        
-        //Find out the start day of the current month, monday=1, sunday=7    
-        $start_day = $date->format('N');        
-                        
-        $current = clone $date;
-                
-        //Work out the number of days to add to the start of the array (i.e starting day - 1)
-        if ($start_day > 1 ){            	
-	    // Rewind the start to a Monday
-            $current->modify('-' . ($start_day-1) . ' day');                       
+    public function show_month($year = false, $month = false) {
+
+        if ( $year === false || $month === false ){
+            $year = date('Y');
+            $month = date('m');
         }
-        	
+
+        $no_days = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        $date = new DateTime($year.'-'.$month.'-01');
+
+        //Find out the start day of the current month, monday=1, sunday=7
+        $start_day = $date->format('N');
+
+        $current = clone $date;
+
+        //Work out the number of days to add to the start of the array (i.e starting day - 1)
+        if ($start_day > 1 ){
+	    // Rewind the start to a Monday
+            $current->modify('-' . ($start_day-1) . ' day');
+        }
+
         $end = clone $current;
         $end->modify('+6 week');
-        
+
         var_dump($end);
-        
+
         $calendar = array();
         $calendar['current_month']              = $date->format('m');
         $calendar['current_month_short']        = $date->format('M');
         $calendar['current_month_long']         = $date->format('F');;
-        
+
         $calendar['current_year_full']          = $date->format('Y');
         $calendar['current_year_short']         = $date->format('y');
-        
+
         $next_month = clone $date;
         $next_month->modify('+1 month');
-        
+
         $calendar['next_month']                 = $next_month->format('m');
         $calendar['next_month_short']           = $next_month->format('M');
         $calendar['next_month_long']            = $next_month->format('F');
-        
+
         $prev_month = clone $date;
         $prev_month->modify('-1 month');
-        
+
         $calendar['prev_month']                 = $prev_month->format('m');
         $calendar['prev_month_short']           = $prev_month->format('M');
-        $calendar['prev_month_long']            = $prev_month->format('F');        
-                
+        $calendar['prev_month_long']            = $prev_month->format('F');
+
         $calendar['absolute_start_date_full']   = $current->format('Y-m-d');
         $calendar['absolute_start_date_month']  = $current->format('m');
         $calendar['absolute_start_date_year']   = $current->format('Y');
-        
+
         $calendar['absolute_end_date_full']     = $end->format('Y-m-d');
         $calendar['absolute_end_date_month']    = $end->format('m');
         $calendar['absolute_end_date_year']     = $end->format('Y');
-                
+
         $calendar['days_in_month']              = $no_days;
         $calendar['first_day_of_month']         = $start_day;
-                
+
         $date_cycle = clone $current;
-        
+
         $i=0;
-        while ($date_cycle < $end){                                    
-                        
+        while ($date_cycle < $end){
+
             $calendar['dates'][$i] = array (
                 'date_sql'      =>  $date_cycle->format('Y-m-d'),
                 'date_readable' =>  $date_cycle->format('d-m-Y'),
                 'date'          =>  $date_cycle->format('d'),
                 'month'         =>  $date_cycle->format('m'),
-                'month_short'   =>  $prev_month->format('M'), 
+                'month_short'   =>  $prev_month->format('M'),
                 'month_long'    =>  $prev_month->format('F'),
                 'year'          =>  $date_cycle->format('Y'),
                 'day_no'        =>  $date_cycle->format('N'),
                 'day_short'     =>  $date_cycle->format('D'),
-                'day_long'      =>  $date_cycle->format('l'),                                
+                'day_long'      =>  $date_cycle->format('l'),
                 'data'          =>  ''
-                
+
             );
-            
-            $date_cycle->modify('+1 day'); 
+
+            $date_cycle->modify('+1 day');
             $i++;
-        }           
-        
+        }
+
         $this->calendar = $calendar;
     }
-    
+
     private function set_readable_names(){
-        
+
         $this->month_names = array(
                 __('month_jan', "January"),
                 __('month_feb', "February"),
@@ -131,11 +129,11 @@ class calendar extends base {
                 __('day_friday_short', "Fri"),
                 __('day_saturday_short', "Sat")
         );
-        
+
     }
-    
-    
-    
+
+
+
 
 
 
